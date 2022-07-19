072add54eae4988065b9d930d88e54b423287c55
Dhruvik <81076631+dhruviktank@users.noreply.github.com>
2022-07-19 15:00:50 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "database1";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$number = $_POST['number'];
+$address = $_POST['address'];
+
+
+$sql = "INSERT INTO table1 (name, email, phone, address) VALUES ('$name','$email','$number','$address')";
+$conn = new mysqli($servername,$username,$password,$database);
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+ }
+ else{
+ echo "Connected successfully";
+ }
+if($conn->query($sql)===TRUE){
+   echo "data inserted successfully";
+} else {
+   echo "Error: " . $sql . "<br>" . $conn->error;
+ }
